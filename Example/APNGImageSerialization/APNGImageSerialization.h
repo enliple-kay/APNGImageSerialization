1f9ce60a39aabcff73781eb799205e5b9afecbb0
kay <ickang@enliple.com>
2020-10-15 16:24:10 +0900

    Supported carthage

---
+++ Example/APNGImageSerialization/APNGImageSerialization.h
@@ -0,0 +1,19 @@
+//
+//  APNGImageSerialization.h
+//  APNGImageSerialization
+//
+//  Created by kay on 2020/10/15.
+//  Copyright © 2020 Ricky Tan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APNGImageSerialization.
+FOUNDATION_EXPORT double APNGImageSerializationVersionNumber;
+
+//! Project version string for APNGImageSerialization.
+FOUNDATION_EXPORT const unsigned char APNGImageSerializationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APNGImageSerialization/PublicHeader.h>
+
+
